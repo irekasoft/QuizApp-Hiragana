609b112438447f88a844f6026aba269d12bf9f25
Muhammad Hijazi <hijazi@irekasoft.com>
2013-09-29 13:27:28 +0800

    Initial Commit

---
+++ QuizApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QuizApp
+//
+//  Created by Muhammad Hijazi  on 29/9/13.
+//  Copyright (c) 2013 iReka Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QuizApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QuizApp
+//
+//  Created by Muhammad Hijazi  on 29/9/13.
+//  Copyright (c) 2013 iReka Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
